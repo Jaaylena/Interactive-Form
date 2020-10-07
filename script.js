471c18b77c1d5670de72541c8e9f0506bfa21cb8
Jelena Feliciano <jfel217@gmail.com>
2020-10-07 13:31:57 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+//create a text field with focus upon loading 
+
+//job role section 
+
+
+//include a text field that appears when user selects "other" in the 
+//job role drop down 
+    //give feild an id "other-title" 
+    // add place-holder text "your job role"
