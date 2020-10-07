d7c084cb13a8ec0fab680ba82cb42f7ee9a69bba
Jelena Feliciano <jfel217@gmail.com>
2020-10-07 17:18:52 -0400

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,23 @@
 //job role drop down 
     //give feild an id "other-title" 
     // add place-holder text "your job role"
+    //hide the "other" initially in order for this feature to work when JS is disabled
+
+
+//T-shirt section 
+//select and options elements do warm up for better understanding 
+    //select design theme 
+        // if no theme in the "design" menu
+              //no color options appear in the "color" dropdown 
+             //color field reads "Please select a T-shirt theme"
+    //T-shirt "color" menu 
+        //display options that match the users theme selection
+        //if user selects "Theme- I Puns"
+            //the color menu should display "cornflower blue", 
+            //"dark slate grey", and "gold"
+        //if user selects "theme- I heart JS" 
+            //color menu should display "Tomatoe", "steel blue",
+            //and "dim grey"
+//update the color field and drop down menu when a new theme is selected
+    //in the design.
+//register for activities section 
