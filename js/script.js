3a012a505f615a95e85304d4d23fba12ad08c0bf
Jelena Feliciano <jfel217@gmail.com>
2020-10-14 17:20:25 -0400

    Update script.js
    not sure how to separte colors based on theme

--- js/script.js
+++ js/script.js
@@ -18,24 +18,30 @@ title.addEventListener('change', (e) => {
     }   
 });
 //T-shirt section 
-const selectDesign = document.getElementById('design');
+const selectDesign = document.querySelector('#design');
 const colors = document.getElementById('color');
+const colorOptions = document.querySelectorAll('color option');
+console.log(colorOptions);
 //at initial load update the "color" field to read "Please select a Theme"
 const colorPlaceholder = document.createElement('option');
     colorPlaceholder.style.display = 'none';
-    colors.appendChild(colorPlaceholder).text = 'Please Select a Theme';
-    colors.value = 'Please Select a Theme';
-console.log(selectDesign);
-console.log(colors);
-//filter the available "color" options by the selected theme in the design field
-for(i = 0; i <= colors.length; i++) {
+    colors.appendChild(colorPlaceholder).text = 'Please Select a T-Shirt Theme';
+    colors.value = 'Please Select a T-Shirt Theme';
+
+for(let i = 0; i < colors.length; i++) {
    //hide the colors in the "color" drop down    
     colors[i].style.display = 'none';
 }
-    //when a design theme 
-        // if no theme in the "design" menu
-              //no color options appear in the "color" dropdown 
-             //color field reads "Please select a T-shirt theme"
+//filter the available "color" options by the selected theme in the design field
+function punColorOptions (option) {
+    colorOptions[0];
+    colorOptions[1];
+    colorOptions[2];
+}
+console.log(punColorOptions());
+    //when a design theme is selected show the colors for that theme
+
+        //color field reads "Please select a T-shirt theme"
     //T-shirt "color" menu 
         //display options that match the users theme selection
         //if user selects "Theme- I Puns"
