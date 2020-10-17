0c86bd9017be7afe1f3fb0868d4867d52c6d9d87
JelenaMF <jfel217@gmail.com>
2020-10-16 22:30:27 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -20,33 +20,30 @@ title.addEventListener('change', (e) => {
 //T-shirt section 
 const selectDesign = document.querySelector('#design');
 const colors = document.getElementById('color');
-const colorOptions = document.querySelectorAll('color option');
-console.log(colorOptions);
 //at initial load update the "color" field to read "Please select a Theme"
 const colorPlaceholder = document.createElement('option');
     colorPlaceholder.style.display = 'none';
     colors.appendChild(colorPlaceholder).text = 'Please Select a T-Shirt Theme';
     colors.value = 'Please Select a T-Shirt Theme';
-
 for(let i = 0; i < colors.length; i++) {
    //hide the colors in the "color" drop down    
     colors[i].style.display = 'none';
 }
-//filter the available "color" options by the selected theme in the design field
-function punColorOptions (option) {
-    colorOptions[0];
-    colorOptions[1];
-    colorOptions[2];
-}
-console.log(punColorOptions());
-    //when a design theme is selected show the colors for that theme
 
-        //color field reads "Please select a T-shirt theme"
-    //T-shirt "color" menu 
-        //display options that match the users theme selection
-        //if user selects "Theme- I Puns"
-            //the color menu should display "cornflower blue", 
+//when a design theme is selected show the colors for that theme
+selectDesign.addEventListener('change', (e) => {
+//filter the available "color" options by the selected theme in the design field
+     //if user selects "Theme- I Puns"
+    if(e.target.value === 'js puns') {
+     //display options that match the users theme selection
+    
+    }
+     //the color menu should display "cornflower blue", 
             //"dark slate grey", and "gold"
+    
+});
+        
+           
         //if user selects "theme- I heart JS" 
             //color menu should display "Tomatoe", "steel blue",
             //and "dim grey"
