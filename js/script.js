afcd191e5f286a1de265fbebd9df797813902e38
JelenaMF <jfel217@gmail.com>
2020-10-18 18:20:07 -0400

    Update script.js
    corrected the issue I was having my options

--- js/script.js
+++ js/script.js
@@ -1,20 +1,20 @@
 //select element with id name and set it's focus
-     document.getElementById('name').focus();
+document.getElementById('name').focus();
 
 /***job role section **/
 //**hide the "other" initially in order for this feature to work when JS is disabled
- 
+
 const otherInput = document.getElementById('other-title');
-    otherInput.style.display = 'none';
+otherInput.style.display = 'none';
 const title = document.getElementById('title');
 let titleValue = title.value;
 //when "other" job role is selected in the drop down 
 title.addEventListener('change', (e) => {
-    if(e.target.value === 'other') {
+    if (e.target.value === 'other') {
         otherInput.style.display = 'block';
     } else {
         otherInput.style.display = 'none';
-    }       
+    }
 });
 //T-shirt section 
 const designMenu = document.querySelector('#design');
@@ -24,43 +24,39 @@ console.log(colorOption);
 
 //at initial load update the "color" field to read "Please select a Theme"
 const colorPlaceholder = document.createElement('option');
-    colorPlaceholder.style.display = 'none';
-    colors.appendChild(colorPlaceholder).text = 'Please Select a T-Shirt Theme';
-    colors.value = 'Please Select a T-Shirt Theme';
+colorPlaceholder.style.display = 'none';
+colors.appendChild(colorPlaceholder).text = 'Please Select a T-Shirt Theme';
+colors.value = 'Please Select a T-Shirt Theme';
 
-for(let i = 0; i < colors.length; i++) {
-   //hide the colors in the "color" drop down    
-    colors[i].style.display = 'none';  
+for (let i = 0; i < colors.length; i++) {
+    //hide the colors in the "color" drop down    
+    colors[i].style.display = 'none';
 }
 //when a design theme is selected show the colors for that theme
 designMenu.addEventListener('change', (e) => {
+    //when a theme is selected drop down placeholder changes to 'choose a color' 
     colors.appendChild(colorPlaceholder).text = 'Choose a Color';
-    colors.value = 'Choose a Color';//filter the available "color" options by the selected theme in the design field
-     //if user selects "Theme- I Puns"
-    if(e.target.value === 'js puns') {
-      colorOption[0].style.display = 'block';
-      colorOption[1].style.display = 'block';
-      colorOption[2].style.display = 'block';
-     //display options that match the users theme selection
-    } else {
-        for(let i = 0; i < colors.length; i++) {
-            //hide the colors in the "color" drop down    
-             colors[i].style.display = 'none';  
-         }
-    } if(e.target.value === 'heart js') {
-        colorOption[3].style.display = 'block';
-        colorOption[4].style.display = 'block';
-        colorOption[5].style.display = 'block';
-    }
-     //the color menu should display "cornflower blue", 
-            //"dark slate grey", and "gold"
-    
-});
-        
-           
+    colors.value = 'Choose a Color'; 
+    //filter the available "color" options by the selected theme in the design field
+    for (let i = 0; i < colors.length; i++) {
+        //hide the colors in the "color" drop down    
+        colors[i].style.display = 'none';
+
+        //if user selects "Theme- I Puns"
+        if (e.target.value === 'js puns') {
+            //the color menu should display "cornflower blue", "dark slate grey", and "gold"
+            colorOption[0].style.display = 'block';
+            colorOption[1].style.display = 'block';
+            colorOption[2].style.display = 'block';
+            //display options that match the users theme selection
+        }
         //if user selects "theme- I heart JS" 
-            //color menu should display "Tomatoe", "steel blue",
-            //and "dim grey"
-//update the color field and drop down menu when a new theme is selected
-    //in the design.
-//register for activities section 
+        if (e.target.value === 'heart js') {
+            //color menu should display "Tomatoe", "steel blue", and "dim grey"
+            colorOption[3].style.display = 'block';
+            colorOption[4].style.display = 'block';
+            colorOption[5].style.display = 'block';
+        }
+    }
+});
+//register for activities section
