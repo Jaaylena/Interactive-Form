cfb8ac38ca89932175316423711608759f38f1d2
JelenaMF <jfel217@gmail.com>
2020-10-17 21:07:49 -0400

    Update script.js
    only one of the three color options show when the first theme is selected.

--- js/script.js
+++ js/script.js
@@ -11,11 +11,7 @@ const title = document.getElementById('title');
 //when "other" job role is selected in the drop down 
 title.addEventListener('change', (e) => {
     //show text input with ID 'other-title'
-    if(e.target.value === 'other') {
-        otherInput.style.display = 'block';
-    }else {
-        otherInput.style.display = 'none';
-    }   
+   
 });
 //T-shirt section 
 const selectDesign = document.querySelector('#design');
@@ -27,16 +23,33 @@ const colorPlaceholder = document.createElement('option');
     colors.value = 'Please Select a T-Shirt Theme';
 for(let i = 0; i < colors.length; i++) {
    //hide the colors in the "color" drop down    
-    colors[i].style.display = 'none';
+    colors[i].style.display = 'none';  
 }
 
 //when a design theme is selected show the colors for that theme
 selectDesign.addEventListener('change', (e) => {
+    for(let i = 0; i < colors.length; i++) {
+        //hide the colors in the "color" drop down    
+         colors[i].style.display = 'none';  
+     }
 //filter the available "color" options by the selected theme in the design field
+
+let loveJsOptions = {
+    "one" : colors.value = 'tomato',
+    "two" : colors.value = 'steelblue',
+    "three" : colors.value = 'dimgrey'
+};
      //if user selects "Theme- I Puns"
     if(e.target.value === 'js puns') {
      //display options that match the users theme selection
-    
+     let punOptions = {
+        "one" : colors.value = 'cornflowerblue',
+        "two" : colors.value = 'darkslategrey',
+        "three" : colors.value = 'gold'
+    };
+    console.log(punOptions);
+
+
     }
      //the color menu should display "cornflower blue", 
             //"dark slate grey", and "gold"
