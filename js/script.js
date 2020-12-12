6827acf2db26529f8c602057ebd48e85cdbf6b61
Jelena Feliciano <jfel217@gmail.com>
2020-12-12 01:25:32 -0500

    js/css
    Validations should be working now.

--- js/script.js
+++ js/script.js
@@ -10,70 +10,29 @@ document
     "afterbegin",
     `<p class="reqField">* - required field</p>`
   );
+
+/***job role section **/
 const otherInput = document.getElementById("other-title");
 otherInput.style.display = "none";
 const title = document.getElementById("title");
-//created elements to display error messages
-let nameError = document.createElement("div");
-const emailError = document.createElement("div");
-const errorCC = document.createElement("div");
-const zipError = document.createElement("div");
-const errorCVV = document.createElement("div");
-const pickActivity = document.createElement("div");
-//variables for separating the payment methods
-const payments = document.querySelector("#payment");
-const paymentOption = document.querySelectorAll("#payment option");
-const creditCard = document.querySelector("#credit-card");
-const payPal = document.querySelector("#paypal");
-const bitcoin = document.querySelector("#bitcoin");
-paymentOption[0].hidden = true;
-payments.value = "credit card";
-payPal.style.display = "none";
-bitcoin.style.display = "none";
-const designMenu = document.querySelector("#design");
+title.addEventListener("change", (e) => {
+  e.target.value === "other" ?
+    (otherInput.style.display = "block") :
+    (otherInput.style.display = "none");
+});
+//T-shirt section
 const colors = document.getElementById("color");
+for (let i = 0; i < colors.length; i++) {
+  //hide the colors in the "color" drop down
+  colors[i].style.display = "none";
+}
+const designMenu = document.querySelector("#design");
 const colorOption = document.querySelectorAll("#color option");
 //at initial load update the "color" field to read "Please select a Theme"
 const colorPlaceholder = document.createElement("option");
 colorPlaceholder.style.display = "none";
 colors.appendChild(colorPlaceholder).text = "Please Select a T-Shirt Theme";
 colors.value = "Please Select a T-Shirt Theme";
-const ccNum = document.getElementById("cc-num");
-const cvvLabel = document.querySelectorAll(".col-3 label")[1];
-cvvLabel.innerHTML = "*cvv";
-const expDate = document.querySelectorAll(".credit-card label")[3];
-expDate.innerHTML = "*Expiration Date:";
-const expYear = document.querySelectorAll(".credit-card label")[4];
-expYear.innerHTML = "*Expiration Year:";
-const ccLabel = document.querySelector(".col-6 label");
-ccLabel.innerHTML = "*Card Number:";
-const totalCostDiv = document.createElement("div");
-totalCostDiv.id = "total-cost";
-const totalCostLabel = document.createElement("label");
-totalCostDiv.append(totalCostLabel);
-let totalCost = 0;
-totalCostLabel.innerHTML = `Total Cost: $${totalCost}`;
-const activities = document.querySelector(".activities");
-activities.appendChild(totalCostDiv);
-const checkboxes = document.querySelectorAll('input[type="checkbox"]');
-const email = document.getElementsByTagName("label")[1];
-email.innerHTML = "*Email:";
-const name = document.getElementsByTagName("label")[0];
-name.innerHTML = "*Name:";
-const zip = document.getElementById("zip");
-const zipLabel = document.querySelectorAll(".col-3 label")[0];
-zipLabel.innerHTML = "*Zip Code:";
-/***job role section **/
-title.addEventListener("change", (e) => {
-  e.target.value === "other"
-    ? (otherInput.style.display = "block")
-    : (otherInput.style.display = "none");
-});
-//T-shirt section
-for (let i = 0; i < colors.length; i++) {
-  //hide the colors in the "color" drop down
-  colors[i].style.display = "none";
-}
 //event listener for displaying colors depending on selected theme
 designMenu.addEventListener("change", (e) => {
   colors.appendChild(colorPlaceholder).text = "Choose a Color";
@@ -97,6 +56,17 @@ designMenu.addEventListener("change", (e) => {
   }
 });
 /*register for activities section*/
+//variables that add the total cost to the activites sections 
+const totalCostDiv = document.createElement("div");
+totalCostDiv.id = "total-cost";
+const totalCostLabel = document.createElement("label");
+totalCostDiv.append(totalCostLabel);
+let totalCost = 0;
+totalCostLabel.innerHTML = `Total Cost: $${totalCost}`;
+const activities = document.querySelector(".activities");
+activities.appendChild(totalCostDiv);
+const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+//event listens for the checkbox selection and adds the price to the total
 activities.addEventListener("change", (e) => {
   let checkedBox = e.target;
   const cost = parseInt(checkedBox.getAttribute("data-cost"));
@@ -111,9 +81,9 @@ activities.addEventListener("change", (e) => {
   for (let i = 0; i < checkboxes.length; i++) {
     const dateAndTime = checkboxes[i].getAttribute("data-day-and-time");
     if (dateAndTime === activityTime && checkedBox !== checkboxes[i]) {
-      checkedBox.checked
-        ? (checkboxes[i].disabled = true)
-        : (checkboxes[i].disabled = false);
+      checkedBox.checked ?
+        (checkboxes[i].disabled = true) :
+        (checkboxes[i].disabled = false);
     }
   }
 });
@@ -125,6 +95,16 @@ activities.addEventListener("change", (e) => {
   });
 });
 /********* payment section ********/
+//variables for separating the payment methods
+const payments = document.querySelector("#payment");
+const paymentOption = document.querySelectorAll("#payment option");
+const creditCard = document.querySelector("#credit-card");
+const payPal = document.querySelector("#paypal");
+const bitcoin = document.querySelector("#bitcoin");
+paymentOption[0].hidden = true;
+payments.value = "credit card";
+payPal.style.display = "none";
+bitcoin.style.display = "none";
 //show/hide pym options depending on the users selection
 payment.addEventListener("change", (e) => {
   if (e.target.value === "credit card") {
@@ -144,47 +124,79 @@ payment.addEventListener("change", (e) => {
   }
 });
 /** form Validation section */
+//variables that add the * to each label
+const name = document.getElementsByTagName("label")[0];
+name.innerHTML = "*Name:";
+const email = document.getElementsByTagName("label")[1];
+email.innerHTML = "*Email:";
+const cvvLabel = document.querySelectorAll(".col-3 label")[1];
+cvvLabel.innerHTML = "*cvv";
+const expDate = document.querySelectorAll(".credit-card label")[3];
+expDate.innerHTML = "*Expiration Date:";
+const expYear = document.querySelectorAll(".credit-card label")[4];
+expYear.innerHTML = "*Expiration Year:";
+const ccLabel = document.querySelector(".col-6 label");
+ccLabel.innerHTML = "*Card Number:";
+const zipLabel = document.querySelectorAll(".col-3 label")[0];
+zipLabel.innerHTML = "*Zip Code:";
+//creating error messages
+function createError(formField, message) {
+  if(!formField.previousElementSibling.classList.contains('error')) {
+    const errorMessage = document.createElement('div');
+    const errorText = document.createTextNode(message);
+    errorMessage.appendChild(errorText);
+    errorMessage.className = 'error';
+    formField.style.display.backgroundColor = 'red';
+    formField.parentElement.insertBefore(errorMessage, formField);
+  }
+}
+function removeError(formField) {
+  if(formField.previousElementSibling.classList.contains('error')) {
+    formField.previousElementSibling.remove();
+  }
+}
 function isNameValid() {
   const nameInput = document.getElementById("name");
-  nameInput.insertAdjacentElement("beforebegin", nameError);
-  const regName = /^(\w+)?$/i;
-  if (!regName.test(nameInput.value)) {
-    nameInput.setAttribute("required", true);
-    nameError.textContent = "* Please enter your first name";
-    nameError.style.color = "#250D54";
-    console.log(nameError);
-    return false; 
-    } else {
-    nameInput.setAttribute("required", false);
-    nameError.textContent = "";
-    
-    return true;
+  if(nameInput.value === '') {
+    removeError(nameInput);
+    createError(nameInput, '* Please Enter Your Name');
+    return false;
   } 
+  if(nameInput.value.search(/\d/) != -1) {
+    removeError(nameInput);
+    createError(nameInput, '* Name cannot contain numbers');
+  } else {
+    removeError(nameInput);
+    return true;  
+  }
 }
 
 function isEmailValid() {
-  const regEmail = /^[^@]+@[^@.]+\.[\w+]+$/i;
-  const emailInput = document.getElementById("mail");
-  emailInput.insertAdjacentElement("beforebegin", emailError);
-  if (!regEmail.test(emailInput.value)) {
-    emailInput.setAttribute("required", true);
-    emailError.textContent = "*Please enter a valid Email address";
-    emailError.style.color = "#250D54";
-    return false;
-  } else {
-    emailInput.setAttribute("required", false);
-    emailError.textContent = "";
-    return true;
-  }
-}
+     const regEmail = /^[^@]+@[^@.]+\.[\w+]+$/i;
+     const emailInput = document.getElementById("mail");
+    if(emailInput.value === '') {
+      removeError(emailInput);
+      createError(emailInput, '* Email is Empty');
+      return false;
+    } 
+    if (!regEmail.test(emailInput.value)) {
+      removeError(emailInput);
+      createError(emailInput, '* Please enter a valid Email Address');
+      return false; 
+    } else {
+      removeError(emailInput);
+      return true;
+    }
+   }
+const pickActivity = document.createElement("div");
+const checkbox1 = document.querySelectorAll(".activities label")[0];
+activities.insertBefore(pickActivity, checkbox1);
 function checkActivities() {
-  const checkbox1 = document.querySelectorAll(".activities label")[0];
   for (let i = 0; i < checkboxes.length; i++) {
     if (!checkboxes[i].checked) {
       checkboxes[i].setAttribute("required", true);
       pickActivity.textContent = "* Must Select Main conference.";
       pickActivity.style.color = "red";
-      activities.insertBefore(pickActivity, checkbox1);
       return false;
     } else {
       pickActivity.textContent = "";
@@ -192,69 +204,51 @@ function checkActivities() {
     }
   }
 }
+
 function isCcValid() {
+  const ccNum = document.getElementById("cc-num");
   const regexNums = /^(\d{13,16})$/;
-  if (!regexNums.test(ccNum.value)) {
-    ccNum.setAttribute("required", true);
-    errorCC.textContent = " *should contain 13-16 digits";
-    errorCC.style.color = "red";
-    ccLabel.append(errorCC);
-    return false;
-  } else {
-    ccNum.setAttribute("required", false);
-    errorCC.textContent = "";
-    return true;
-  }
-}
-function isZipValid() {
+  const zip = document.getElementById("zip");
   const regexZip = /\b\d{5}\b/;
-  if (!regexZip.test(zip.value)) {
-    zip.setAttribute("required", true);
-    zipLabel.append(zipError);
-    zipError.textContent = " *Invalid zip code";
-    zipError.style.color = "red";
-    return false;
-  } else {
-    zip.setAttribute("required", false);
-    zipError.textContent = "";
-    return true;
-  }
-}
-function isCvvValid() {
   const cvvInput = document.getElementById("cvv");
   const regexCvv = /^\d{3}$/;
-  if (!regexCvv.test(cvvInput.value)) {
-    cvvInput.setAttribute("required", true);
-    errorCVV.style.color = "red";
-    errorCVV.textContent = " *3 digits on back";
-    cvvLabel.append(errorCVV);
-    return true;
-  } else {
-    cvvInput.setAttribute("required", false);
-    errorCVV.textContent = "";
-    return false;
-  }
-}
-function validateForm() {
-  const formFields = [isNameValid(), isEmailValid(), checkActivities()];
   if(payments.value === 'credit card') {
-    formFields.push(isCcValid(), isZipValid(), isCvvValid());
+    let validate = true;
+    if(!regexNums.test(ccNum.value)) {
+      createError(ccNum, '*Please Enter a Valid Credit Card');
+      validate = false;
+    } else {
+      removeError(ccNum);
+    }
+    if(!regexZip.test(zip.value)){
+      createError(zip, '*Invalid zip code');
+      validate = false;
+    } else {
+      removeError(zip);
+    }
+    if(!regexCvv.test(cvvInput.value)) {
+      createError(cvvInput, '*3 digits on back')
+      validate = false;
+    } else {
+      removeError(cvvInput);
+    }
   }
-  for(let i = 0; i < formFields.length; i++) {
-    if(formFields[i] === false) {
-      return false;
-    } 
-    return true;
-  }
+  return true;
 }
 //an eventlistener that calls tha validate funtions and verifies fieldsets
 document.getElementById("name").addEventListener("keyup", isNameValid);
 document.getElementById("mail").addEventListener("keyup", isEmailValid);
 document.getElementById("cc-num").addEventListener("keyup", isCcValid);
-document.getElementById("zip").addEventListener("keyup", isZipValid);
-document.getElementById("cvv").addEventListener("keyup", isCvvValid);
+document.getElementById('zip').addEventListener('keyup', isCcValid);
+document.getElementById('cvv').addEventListener('keyup', isCcValid)
 submitButton.addEventListener("click", (e) => {
-  if(!validateForm()) {
+  if (isNameValid() && isEmailValid && isCcValid() && checkActivities()) {
+    console.log('submission processed');
+  } else {
     e.preventDefault();
+    isNameValid();
+    isEmailValid();
+    isCcValid();
+    checkActivities();
   }
 });
